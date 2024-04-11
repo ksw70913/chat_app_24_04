c88af8ab85a2b958310a4eda0f5b11d183b308ad
ksw70913 <pavico0913@gmail.com>
2024-04-11 16:38:58 +0900

    GET /chat/messages

--- src/main/java/com/koreait/exam/chat_app_24_04/ChatMessage.java
+++ src/main/java/com/koreait/exam/chat_app_24_04/ChatMessage.java
@@ -1,5 +1,6 @@
 package com.koreait.exam.chat_app_24_04;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,20 +10,21 @@ import java.time.LocalDateTime;
 @Getter
 public class ChatMessage {
   private long id;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
   private LocalDateTime createTime;
   private String authorName;
   private String content;
 
   public ChatMessage(String authorName, String content) {
-    this(ChatMessageIdGenerator.getNextId(),LocalDateTime.now(),authorName,content);
+    this(ChatMessageIdGenerator.getNextId(), LocalDateTime.now(), authorName, content);
   }
 
 }
 
-class ChatMessageIdGenerator{
+class ChatMessageIdGenerator {
   public static long id = 0;
 
-  public static long getNextId(){
+  public static long getNextId() {
     return ++id;
   }
 }
